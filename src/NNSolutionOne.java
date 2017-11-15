e93d05c1cde4932cee18695159b1581229557b17
Koverm <koverm0414@gmail.com>
2017-11-15 18:36:24 +0100

    NNSolOne

---
+++ src/NNSolutionOne.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class NNSolutionOne {
+
+    ArrayList<Integer> input =new ArrayList();
+    Random r= new Random(1);
+    ArrayList<float[][]> m= new ArrayList<>();
+
+    public void read() throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        String tmp= br.readLine();
+        String []tmp2 = tmp.split(",");
+        for (String s: tmp2
+             ) {
+            input.add(Integer.parseInt(s));
+        }
+    }
+
+    public void createMatrix(){
+        for (int i=0;i < input.size()-1;++i){
+            float[][] tmp = new float[input.get(i)][input.get(i+1)];
+            for (int j=0;j<input.get(i);j++){
+                for (int k=0;k< input.get(i+1);k++){
+                    tmp [j][k] = (float) r.nextGaussian()*0.1f;
+                }
+            }
+            m.add(tmp);
+        }
+
+    }
+
+    public void write(){
+        for (Integer i:input
+        ) {
+            System.out.print(i);
+            if (!i.equals(input.get(input.size()-1))){
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+        for (int i=0;i < input.size()-1;++i){
+            for (int j=0;j<input.get(i+1);j++) {
+                for (int k = 0; k < input.get(i); k++) {
+                    System.out.print( m.get(i)[k][j]+",");
+                }
+                System.out.println("0");
+            }
+        }
+    }
+
+        public static void main(String[] args)throws Exception{
+            NNSolutionOne nnso= new NNSolutionOne();
+            nnso.read();
+            nnso.createMatrix();
+            nnso.write();
+        }
+
+}
